309cc6d91633b9a8778fa0e8ca6929c9de83c6e9
FabienMeunier <meunier.fabien@yahoo.fr>
2023-03-03 21:48:04 +0100

    create models & templates

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band, Listing
 
-# Create your views here.
+def hello(request):
+    bands = Band.objects.all()
+    return render(request, 
+                  "listings/hello.html", 
+                  {"bands": bands})
+
+def about(request):
+    return render(request, "listings/about.html")
+
+def contact(request):
+    return render(request, "listings/contact.html")
+
+def list(request):
+    ads = Listing.objects.all()
+    return render(request, 
+                  "listings/list.html", 
+                  {"ads": ads})
