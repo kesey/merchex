b20a15b660089d48de01b0b4d26432ddf48ed99a
FabienMeunier <meunier.fabien@yahoo.fr>
2023-03-07 23:55:43 +0100

    add class Form

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from listings.models import Band, Listing
+from listings.forms import ContactUsForm
+from django.core.mail import send_mail
 
 def band_list(request):
     bands = Band.objects.all()
@@ -20,7 +22,25 @@ def about(request):
     return render(request, "listings/about.html")
 
 def contact(request):
-    return render(request, "listings/contact.html")
+    if request.method == "POST":
+        form = ContactUsForm(request.POST) # if the form has been submit (POST method) populate input with data
+        if form.is_valid():
+            send_mail(
+                subject=f"Message from {form.cleaned_data['name']} via Merchex Contact Us Form",
+                message=form.cleaned_data["message"],
+                from_email=form.cleaned_data["email"],
+                recipient_list=["admin@merchex.xyz"]
+            )
+            return redirect('email-sent') # redirection to confirmation page
+    else:
+        form = ContactUsForm() # if you arrive on this view (GET method) leave the input blank
+
+    return render(request,
+                  "listings/contact.html",
+                  {"form": form})
+
+def email_sent(request):
+    return render(request, "listings/email_confirmation.html")
 
 def list(request):
     lists = Listing.objects.all()
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -21,8 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bands/', views.band_list, name="band-list"),
     path('bands/<int:band_id>/', views.band_detail, name="band-detail"), # with name you can refer to this path in the templates {% url 'name' %}
-    path('about-us/', views.about),
-    path('contact-us/', views.contact),
+    path('about-us/', views.about, name="about"),
+    path('contact-us/', views.contact, name="contact"),
     path('listings/', views.list, name="list-list"),
-    path('listings/<int:list_id>/', views.list_detail, name="list-detail") # <int:list_id> to pass the id to the function in views.list_detail(request, list_id)
+    path('listings/<int:list_id>/', views.list_detail, name="list-detail"), # <int:list_id> to pass the id to the function in views.list_detail(request, list_id)
+    path('email-confirmation/', views.email_sent, name="email-sent")
 ]
